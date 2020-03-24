3f8f5035ff15bd895f650e8d6b1a109bf0572db8
churchan <qiuchengswufe@vip.qq.com>
2020-03-24 07:59:54 +0000

    Add fragment

---
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -0,0 +1,44 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+    private int id;
+    private String countyNmae;
+    private String weatherId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyNmae() {
+        return countyNmae;
+    }
+
+    public void setCountyNmae(String countyNmae) {
+        this.countyNmae = countyNmae;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    private int cityId;
+
+}
