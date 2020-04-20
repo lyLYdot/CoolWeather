34de6c2b9cf7eca4e3c59fa6dc126e6478afd5d3
churchan <qiuchengswufe@vip.qq.com>
2020-04-20 13:08:12 +0000

    Add Bing background

--- app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -1,6 +1,7 @@
 package com.coolweather.android;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -138,6 +139,21 @@ public class ChooseAreaFragment extends Fragment {
                     queryCounties();
 
                 }
+
+                else if(currentLevel==LEVEL_COUNTY){
+
+                    String weatherId = countyList.get(position).getWeatherId();
+
+                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
+
+                    intent.putExtra("weather_id",weatherId); // 向intent传入WeatherId
+
+                    startActivity(intent);
+
+                    getActivity().finish();
+
+                }
+
 
             }
 
